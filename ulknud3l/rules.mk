a15d10580083ace0fd94389d1c8fa81fdc56a9a1
UlkNudel <wotzlawskijonas@gmail.com>
2024-06-20 10:14:37 +0200

    added keymap

---
+++ ulknud3l/rules.mk
@@ -0,0 +1,7 @@
+OLED_ENABLE = no
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
+LEADER_ENABLE = yes        # Enable the Leader Key feature
+MOUSEKEY_ENABLE = no
+CONVERT_TO = liatris
+NKRO_ENABLE = yes
